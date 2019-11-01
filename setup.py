2af3b96e4d112ea3e11eebcf6913f3e68b2b70a9
jay ess <mentoce@gmail.com>
2019-11-01 16:58:58 -0700

    Create initial setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='google_image_downloader',
+    version='0.1.0',
+    description='Python Distribution Utilities',
+    author='Atif Ahmed',
+    author_email='atif.ahmed@columbia.edu',
+    url='http://columbia.edu/~aa3931',
+    packages=['google_image_downloader'],
+)
